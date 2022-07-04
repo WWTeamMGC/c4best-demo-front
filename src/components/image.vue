729c67491598276fefa068d50c52e8eae58b2ab2
test <liuyang020207@163.com>
2022-07-04 23:16:38 +0800

    finally

--- src/components/image.vue
+++ src/components/image.vue
@@ -25,7 +25,7 @@
         width="200">
       <template slot-scope="scope">
         <el-button @click="handleClick(scope.row)" type="success" size="big">允许</el-button>
-        <el-button @click="" type="danger" size="big">驳回</el-button>
+        <el-button @click="imgDelete(scope.$index, scope.row)" type="danger" size="big">驳回</el-button>
       </template>
     </el-table-column>
   </el-table>
@@ -39,26 +39,46 @@ export default {
   name: "image",
   methods: {
     handleClick(row) {
-      console.log(row);
+      this.$axios({
+        method:'post',
+        url:'',
+        data:row
+      })
     },
     searchImg(){
       this.$axios({
         method: 'post',
         url: ''
       }).then(response => {
-        this.tableData=response.data.badiplist
+        this.tableData = response.data.url
       })
-    }
+    },
+    imgDelete(index, row) {
+      /*console.log(index, row);*/
+      this.$axios({
+        method:'post',
+        url:'',
+        data:row
+      }).then(response=>{
+        //删除成功
+        if (response.data == 1){
+          this.searchImg()
+        }else {
+          alert("系统繁忙，请稍后重试.....")
+        }
+      })
+    },
+
   },
   created() {
     this.searchImg()
   },
   data() {
     return {
-      fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
+      /*fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],*/
       url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
       tableData: [{
-        data: ''
+        url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
       }]
     }
   }
